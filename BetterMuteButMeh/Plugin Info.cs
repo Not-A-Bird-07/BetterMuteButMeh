e13bd80e7f1deeeecdfe91319e4f8edf999ce12b
notabird <notabird07@gmail.com>
2025-04-10 16:34:14 -0700

    version bumb

--- BetterMuteButMeh/Plugin Info.cs
+++ BetterMuteButMeh/Plugin Info.cs
@@ -2,6 +2,6 @@ namespace BetterMuteButMeh
 {
     public class Plugin_Info
     {
-        public const string GUID = "NotABird.Better.Mute.But.Meh", NAME = "Better Mute But Meh", VERSION = "1.0.0";
+        public const string GUID = "NotABird.Better.Mute.But.Meh", NAME = "Better Mute But Meh", VERSION = "1.0.1";
     }
 }
